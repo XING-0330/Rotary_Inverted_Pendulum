ba52b21a9e748f4d5aa0e98d1070513021123f71
XING-0330 <nasanasa5c@gmail.com>
2023-12-30 01:08:05 +0800

    repair lib problem

--- Software/Arduino/main/lowpass_filter.h
+++ Software/Arduino/main/lowpass_filter.h
@@ -1,27 +1,20 @@
-#include <Arduino.h> 
+#include <Arduino.h>
 
 #ifndef LOWPASS_FILTER_H
 #define LOWPASS_FILTER_H
 
-/**
-  * 低通滤波器类
-  */
-  
 class LowPassFilter
 {
 public:
-    /**
-     * @Tf - 低通滤波时间常数
-     */
-    LowPassFilter(float Tf);
-    ~LowPassFilter() = default;
+  LowPassFilter(float Tf);
+  ~LowPassFilter() = default;
 
-    float operator() (float x);
-    float Tf; //!< 低通滤波时间常数
+  float operator()(float x);
+  float Tf;
 
 protected:
-    unsigned long timestamp_prev;  //!< 最后执行时间戳
-    float y_prev; //!< 上一个循环中的过滤后的值
+  unsigned long timestamp_prev;
+  float y_prev;
 };
 
 #endif
